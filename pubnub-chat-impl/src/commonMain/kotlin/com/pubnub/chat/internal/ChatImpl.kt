0c8d08a0580d0f32b4e15b399714778eaad085ae
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-07-16 18:33:51 +0200

    Make sure we're using stringValue for ChannelType

--- pubnub-chat-impl/src/commonMain/kotlin/com/pubnub/chat/internal/ChatImpl.kt
+++ pubnub-chat-impl/src/commonMain/kotlin/com/pubnub/chat/internal/ChatImpl.kt
@@ -903,7 +903,7 @@ class ChatImpl(
             description = updatedChannel.description,
             custom = updatedChannel.custom?.let { createCustomObject(it) },
             includeCustom = false,
-            type = updatedChannel.type.toString().lowercase(),
+            type = updatedChannel.type?.stringValue,
             status = updatedChannel.status
         ).then { pnChannelMetadataResult ->
             pnChannelMetadataResult.data?.let { pnChannelMetadata: PNChannelMetadata ->
@@ -931,7 +931,7 @@ class ChatImpl(
             description = description,
             custom = custom,
             includeCustom = true,
-            type = type?.name?.lowercase(),
+            type = type?.stringValue,
             status = status
         ).then { pnChannelMetadataResult ->
             pnChannelMetadataResult.data?.let { pnChannelMetadata ->
