1052f0fef52fd4966cff5c493d7a79970a79cfd2
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-07-26 09:24:00 +0200

    Change pnsdk string

--- src/jvmMain/kotlin/com/pubnub/chat/mediators.jvm.kt
+++ src/jvmMain/kotlin/com/pubnub/chat/mediators.jvm.kt
@@ -1,5 +1,7 @@
 package com.pubnub.chat
 
+import com.pubnub.api.UserId
+import com.pubnub.api.enums.PNLogVerbosity
 import com.pubnub.api.v2.PNConfiguration
 import com.pubnub.chat.config.ChatConfiguration
 import com.pubnub.chat.internal.ChatImpl
@@ -10,7 +12,17 @@ import com.pubnub.kmp.PNFuture
 fun Chat.Companion.init(chatConfiguration: ChatConfiguration, pubnubConfiguration: PNConfiguration): PNFuture<Chat> {
     val builder = PNConfigurationImpl.Builder(pubnubConfiguration)
     builder.pnsdkSuffixes = pubnubConfiguration.pnsdkSuffixes.toMutableMap().apply {
-        put("chat-sdk", "JVM/$PUBNUB_CHAT_VERSION")
+        put("chat-sdk", "Chat-JVM/$PUBNUB_CHAT_VERSION")
     }
     return ChatImpl(chatConfiguration, com.pubnub.api.PubNub.create(builder.build())).initialize()
+}
+
+fun main() {
+    val pnConfiguration = PNConfiguration.builder(UserId("demo"), "demo").apply {
+        logVerbosity = PNLogVerbosity.BODY
+        publishKey = "demo"
+    }.build()
+    Chat.init(ChatConfiguration(),pnConfiguration).async {
+
+    }
 }
