93d230d87ff86996ff0de106e91c5b35faea92fa
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-09-18 09:45:21 +0200

    Message KDoc

--- src/commonMain/kotlin/com/pubnub/chat/mediators.kt
+++ src/commonMain/kotlin/com/pubnub/chat/mediators.kt
@@ -5,11 +5,23 @@ import com.pubnub.chat.internal.UserImpl
 import com.pubnub.chat.internal.channel.BaseChannel
 import com.pubnub.chat.internal.message.BaseMessage
 
+/**
+ * Receive updates when specific messages and related message reactions are added, edited, or removed.
+ *
+ * @param callback Function that takes a collection of Message objects. It defines the custom behavior to be executed when detecting message or message reaction changes.
+ * @return Interface that lets you stop receiving message-related updates by invoking the close() method
+ */
 fun Message.Companion.streamUpdatesOn(
     messages: Collection<Message>,
     callback: (messages: Collection<Message>) -> Unit,
 ): AutoCloseable = BaseMessage.streamUpdatesOn(messages, callback)
 
+/**
+ * Receive updates when specific messages and related message reactions are added, edited, or removed.
+ *
+ * @param callback Function that takes a collection of ThreadMessage objects. It defines the custom behavior to be executed when detecting message or message reaction changes.
+ * @return Interface that lets you stop receiving message-related updates by invoking the close() method
+ */
 fun ThreadMessage.Companion.streamUpdatesOn(
     messages: Collection<ThreadMessage>,
     callback: (messages: Collection<ThreadMessage>) -> Unit,
