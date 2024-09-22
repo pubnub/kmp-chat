a079bee92931cfbc52f758b8ec2340f3c9ca89e6
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-09-22 16:06:21 +0200

    Change parameter name to pubNubConfiguration

--- src/jvmMain/kotlin/com/pubnub/chat/mediators.jvm.kt
+++ src/jvmMain/kotlin/com/pubnub/chat/mediators.jvm.kt
@@ -7,9 +7,9 @@ import com.pubnub.chat.internal.PUBNUB_CHAT_VERSION
 import com.pubnub.internal.v2.PNConfigurationImpl
 import com.pubnub.kmp.PNFuture
 
-fun Chat.Companion.init(chatConfiguration: ChatConfiguration, pubnubConfiguration: PNConfiguration): PNFuture<Chat> {
-    val builder = PNConfigurationImpl.Builder(pubnubConfiguration)
-    builder.pnsdkSuffixes = pubnubConfiguration.pnsdkSuffixes.toMutableMap().apply {
+fun Chat.Companion.init(chatConfiguration: ChatConfiguration, pubNubConfiguration: PNConfiguration): PNFuture<Chat> {
+    val builder = PNConfigurationImpl.Builder(pubNubConfiguration)
+    builder.pnsdkSuffixes = pubNubConfiguration.pnsdkSuffixes.toMutableMap().apply {
         put("chat-sdk", "CA-JVM/$PUBNUB_CHAT_VERSION")
     }
     return ChatImpl(chatConfiguration, com.pubnub.api.PubNub.create(builder.build())).initialize()
