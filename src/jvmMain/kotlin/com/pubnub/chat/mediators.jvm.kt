7be9f641693a07056f5af5d196cb1c59ab4bb896
marcin.cebo <marcin.cebo@pubnub.com>
2024-10-31 18:05:59 +0100

    Fix creation of Chat when AccessManager enabled on PubNub keys.
    Chat.init() calls initialize() which calls pubNub.getUUIDMetadata that fails when AccessManager is enabled and token is not set for PubNub instance that is created inside Chat.init()
    
    Token should be granted for pubNub.configuration.userId.value
            val token = serverChat.pubNub.grantToken(
                ttl = 1,
                channels = listOf(ChannelGrant.name(get = true, name = "anyChannelForNow")),
                uuids = listOf(UUIDGrant.id(id = pubNub.configuration.userId.value, get = true, update = true)) // this is important
            ).await().token

--- src/jvmMain/kotlin/com/pubnub/chat/mediators.jvm.kt
+++ src/jvmMain/kotlin/com/pubnub/chat/mediators.jvm.kt
@@ -1,15 +1,27 @@
 package com.pubnub.chat
 
+import com.pubnub.api.PubNub
 import com.pubnub.api.v2.PNConfiguration
 import com.pubnub.chat.config.ChatConfiguration
 import com.pubnub.chat.internal.ChatImpl
 import com.pubnub.chat.internal.PUBNUB_CHAT_VERSION
 import com.pubnub.kmp.PNFuture
 
-fun Chat.Companion.init(chatConfiguration: ChatConfiguration, pubNubConfiguration: PNConfiguration): PNFuture<Chat> {
+/**
+ * Initializes an instance of [Chat] with the specified [ChatConfiguration] and [PNConfiguration].
+ * A [token] should be provided when AccessManager is enabled on PubNub keys.
+ *
+ * @param chatConfiguration The configuration for initializing the [Chat] instance.
+ * @param pubNubConfiguration The base PubNub configuration to be used.
+ * @param token Optional authentication token for the PubNub client. Should be provided when AccessManager is enabled on PubNub keys.
+ * @return A [PNFuture] containing the initialized [Chat] instance.
+ */
+fun Chat.Companion.init(chatConfiguration: ChatConfiguration, pubNubConfiguration: PNConfiguration, token: String? = null): PNFuture<Chat> {
     val builder = PNConfiguration.builder(pubNubConfiguration)
     builder.pnsdkSuffixes = pubNubConfiguration.pnsdkSuffixes.toMutableMap().apply {
         put("chat-sdk", "CA-JVM/$PUBNUB_CHAT_VERSION")
     }
-    return ChatImpl(chatConfiguration, com.pubnub.api.PubNub.create(builder.build())).initialize()
+    val pubNub = PubNub.create(builder.build()).apply { setToken(token) }
+
+    return ChatImpl(chatConfiguration, pubNub).initialize()
 }
