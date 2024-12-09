e5fa4be83157365d27a76c5a98b9f0ffddf28e27
Wojtek Kaliciński <146713236+wkal-pubnub@users.noreply.github.com>
2024-12-09 16:07:45 +0100

    Fix kotlin/swift release scripts (#140)
    And disable redundant test & validation on master

--- .github/workflows/release/kotlin-maven-build.sh
+++ .github/workflows/release/kotlin-maven-build.sh
@@ -5,7 +5,7 @@ find build/cocoapods/publish/release/PubNubChat.xcframework -exec touch -t 00000
 rm -f build/cocoapods/publish/release/PubNubChat.xcframework.zip
 zip -X -vr build/cocoapods/publish/release/PubNubChat.xcframework.zip build/cocoapods/publish/release/PubNubChat.xcframework
 touch -t 00000000 build/cocoapods/publish/release/PubNubChat.xcframework.zip
-CHECKSUM=$(swift package compute-checksum /Users/wojciech.kalicinski/projects/pubnub-chat/build/cocoapods/publish/release/PubNubChat.xcframework.zip)
+CHECKSUM=$(swift package compute-checksum build/cocoapods/publish/release/PubNubChat.xcframework.zip)
 echo $CHECKSUM
 mkdir build/gh_artifacts
 mv build/cocoapods/publish/release/PubNubChat.xcframework.zip build/gh_artifacts/
