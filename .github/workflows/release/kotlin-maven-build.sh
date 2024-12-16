97b746a6ad62b0c8d6c380c8f7cf9738c4119a19
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-12-16 09:54:25 +0100

    [CI] Change xcframework bundling scripts

--- .github/workflows/release/kotlin-maven-build.sh
+++ .github/workflows/release/kotlin-maven-build.sh
@@ -3,8 +3,10 @@ echo "Build Kotlin and Swift Chat SDK module artifacts"
 ./gradlew -PENABLE_TARGET_IOS_ALL=true :podPublishReleaseXCFramework assemble
 find build/cocoapods/publish/release/PubNubChat.xcframework -exec touch -t 00000000 {} +
 rm -f build/cocoapods/publish/release/PubNubChat.xcframework.zip
-zip -X -vr build/cocoapods/publish/release/PubNubChat.xcframework.zip build/cocoapods/publish/release/PubNubChat.xcframework
-touch -t 00000000 build/cocoapods/publish/release/PubNubChat.xcframework.zip
+pushd build/cocoapods/publish/release
+zip -X -vr PubNubChat.xcframework.zip PubNubChat.xcframework
+touch -t 00000000 PubNubChat.xcframework.zip
+popd
 CHECKSUM=$(swift package compute-checksum build/cocoapods/publish/release/PubNubChat.xcframework.zip)
 echo $CHECKSUM
 mkdir build/gh_artifacts
