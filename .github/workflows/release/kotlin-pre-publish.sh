76daf7b6b7c4b916e7a3a7bf86308b811d3f9db8
Wojtek Kaliciński <146713236+wkal-pubnub@users.noreply.github.com>
2024-12-16 12:47:38 +0100

    [CI] Change xcframework bundling scripts (#147)

--- .github/workflows/release/kotlin-pre-publish.sh
+++ .github/workflows/release/kotlin-pre-publish.sh
@@ -3,8 +3,10 @@ echo "Update Swift Chat SDK checksum in Package.swift"
 ./gradlew -PENABLE_TARGET_IOS_ALL=true :podPublishReleaseXCFramework
 find build/cocoapods/publish/release/PubNubChat.xcframework -exec touch -t 00000000 {} +
 rm -f build/cocoapods/publish/release/PubNubChat.xcframework.zip
-zip -X -vr build/cocoapods/publish/release/PubNubChat.xcframework.zip build/cocoapods/publish/release/PubNubChat.xcframework
-touch -t 00000000 build/cocoapods/publish/release/PubNubChat.xcframework.zip
+pushd build/cocoapods/publish/release
+zip -X -vr PubNubChat.xcframework.zip PubNubChat.xcframework
+touch -t 00000000 PubNubChat.xcframework.zip
+popd
 CHECKSUM=$(swift package compute-checksum build/cocoapods/publish/release/PubNubChat.xcframework.zip)
 echo $CHECKSUM
 sed -i.bak "s/checksum: \"[a-z0-9]*\"/checksum: \"$CHECKSUM\"/g" Package.swift
