fa92114b7235e272228868cb4a0aaf5a9b8f1741
Wojciech Kaliciński <wojciech.kalicinski@pubnub.com>
2024-12-17 14:42:45 +0100

    [CI] Fix checksum calculation for Swift

--- .github/workflows/release/kotlin-pre-publish.sh
+++ .github/workflows/release/kotlin-pre-publish.sh
@@ -1,11 +1,12 @@
 set -e
+export ZERO_AR_DATE=1
 echo "Update Swift Chat SDK checksum in Package.swift"
 ./gradlew -PENABLE_TARGET_IOS_ALL=true :podPublishReleaseXCFramework
-find build/cocoapods/publish/release/PubNubChat.xcframework -exec touch -t 00000000 {} +
+TZ=UTC find build/cocoapods/publish/release/PubNubChat.xcframework -exec touch -t 197001010000.00 {} +
 rm -f build/cocoapods/publish/release/PubNubChat.xcframework.zip
 pushd build/cocoapods/publish/release
 zip -X -vr PubNubChat.xcframework.zip PubNubChat.xcframework
-touch -t 00000000 PubNubChat.xcframework.zip
+TZ=UTC touch -t 197001010000.00 PubNubChat.xcframework.zip
 popd
 CHECKSUM=$(swift package compute-checksum build/cocoapods/publish/release/PubNubChat.xcframework.zip)
 echo $CHECKSUM
