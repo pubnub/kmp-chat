236584162b2f421241de509c9188a27e636b44fe
jguz-pubnub <jakub.guz@pubnub.com>
2025-01-14 09:07:24 +0100

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     .binaryTarget(
       name: "PubNubChatRemoteBinaryPackage",
       url: "https://github.com/pubnub/kmp-chat/releases/download/kotlin-0.10.1/PubNubChat.xcframework.zip",
-      checksum: "3153a4429665fe51861cfdc3b63cdb873260e777dcb41d52f14301698b2b2a91"
+      checksum: "bfc4a5917e6107df71fd33cec6f2719aa615440ddd8e3e546ce371372b291c4e"
     )
   ]
 )
