376d0a1f1259cd821f60a0f8f64d40d47d66c9b7
marcin.cebo <marcin.cebo@pubnub.com>
2025-06-06 15:43:32 +0200

    Added tests for downloading file.

--- pubnub-chat-impl/src/jsTest/kotlin/com/pubnub/integration/MessageIntegrationTest.js.kt
+++ pubnub-chat-impl/src/jsTest/kotlin/com/pubnub/integration/MessageIntegrationTest.js.kt
@@ -3,16 +3,19 @@ package com.pubnub.integration
 import com.pubnub.kmp.Uploadable
 import com.pubnub.kmp.UploadableImpl
 
-actual fun generateFileContent(): Uploadable {
+actual fun generateFileContent(fileName: String): Uploadable {
+    val fileObj: dynamic = js("({})")
+    fileObj.data = "some text"
+    fileObj.name = fileName
+    fileObj.mimeType = "text/plain"
+    return UploadableImpl(fileObj)
+}
+
+actual fun generateFileContentFromImage(): Uploadable {
+    val base64 = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8/x8AAwMCAO2Z2ioAAAAASUVORK5CYII="
+    // Decode base64 to a Buffer in Node.js
+    val buffer = js("Buffer.from(base64, 'base64')")
     return UploadableImpl(
-        js(
-            """
-        {
-            data: "some text",
-            name: "name.txt",
-            mimeType: "text/plain"
-        }
-    """
-        )
+        js("({ data: buffer, name: 'TestImage01.png', mimeType: 'image/png' })")
     )
 }
