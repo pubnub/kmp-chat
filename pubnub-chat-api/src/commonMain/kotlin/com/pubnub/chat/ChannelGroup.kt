6aee6b3389cac45241cbb64eaa77af41caef9b33
jguz-pubnub <jakub.guz@pubnub.com>
2025-06-23 12:43:44 +0200

    Work in progress

---
+++ pubnub-chat-api/src/commonMain/kotlin/com/pubnub/chat/ChannelGroup.kt
@@ -0,0 +1,93 @@
+package com.pubnub.chat
+
+import com.pubnub.api.models.consumer.objects.PNKey
+import com.pubnub.api.models.consumer.objects.PNPage
+import com.pubnub.api.models.consumer.objects.PNSortKey
+import com.pubnub.chat.types.GetChannelsResponse
+import com.pubnub.kmp.PNFuture
+
+/**
+ * Represents an interface for types that refers to a channel group.
+ */
+interface ChannelGroup {
+    /**
+     * Unique identifier for a channel group.
+     */
+    val id: String
+
+    /**
+     * Reference to the main Chat object.
+     */
+    val chat: Chat
+
+    /**
+     * Returns a paginated list of all existing channels in a given [ChannelGroup].
+     *
+     * @param filter Expression used to filter the results. Returns only these channels whose properties satisfy the given expression are returned.
+     * @param sort A collection to specify the sort order. Available options are id, name, and updated. Use asc or desc.
+     * @param limit Number of objects to return in response. The default (and maximum) value is 100.
+     * @param page Object used for pagination to define which previous or next result page you want to fetch.
+     *
+     * @return [PNFuture] containing [GetChannelsResponse].
+     */
+    fun listChannels(
+        filter: String? = null,
+        sort: Collection<PNSortKey<PNKey>> = listOf(),
+        limit: Int? = null,
+        page: PNPage? = null,
+    ): PNFuture<GetChannelsResponse>
+
+    /**
+     * Adds [Channel] entities to a channel group.
+     *
+     * @param channels Channel entities to add.
+     */
+    fun add(channels: List<Channel>): PNFuture<Unit>
+
+    /**
+     * Adds channels to a channel group.
+     *
+     * This method reduces the overhead of fetching full Channel entities when
+     * only the IDs are known. It does not perform validation to check whether
+     * the channels with the given IDs exist — responsibility for ensuring
+     * validity lies with the caller.
+     *
+     * @param channels [Channel] identifiers to add.
+     */
+    fun add(channels: List<String>): PNFuture<Unit>
+
+    /**
+     * Remove [Channel] entities from a channel group.
+     */
+    fun remove(channels: List<Channel>): PNFuture<Unit>
+
+    /**
+     * Remove channels from a channel group.
+     *
+     * This method reduces the overhead of fetching full Channel entities when
+     * only the IDs are known. It does not perform validation to check whether
+     * the channels with the given IDs exist — responsibility for ensuring
+     * validity lies with the caller.
+     *
+     * @param channels Channel identifiers to add.
+     */
+    fun remove(channels: List<String>): PNFuture<Unit>
+
+    /**
+     * Returns a collection of users currently present in any channel within the given [ChannelGroup]
+     */
+    fun whoIsPresent(): PNFuture<Map<String, List<String>>>
+
+    /**
+     * Enables real-time tracking of users connecting to or disconnecting from the given [ChannelGroup]
+     */
+    fun streamPresence(callback: (userIds: Map<String, List<String>>) -> Unit): AutoCloseable
+
+    /**
+     * Watch the [ChannelGroup] content.
+     *
+     * @param callback custom behavior whenever a message is received
+     * @return AutoCloseable interface you can call to stop listening for new messages by invoking the close() method.
+     */
+    fun connect(callback: (Message) -> Unit): AutoCloseable?
+}
---
+++ pubnub-chat-impl/src/commonMain/kotlin/com/pubnub/chat/internal/channelGroup/ChannelGroupImpl.kt
@@ -0,0 +1,171 @@
+package com.pubnub.chat.internal.channelGroup
+
+import co.touchlab.kermit.Logger
+import com.pubnub.api.models.consumer.objects.PNKey
+import com.pubnub.api.models.consumer.objects.PNPage
+import com.pubnub.api.models.consumer.objects.PNSortKey
+import com.pubnub.api.models.consumer.presence.PNHereNowOccupantData
+import com.pubnub.api.v2.subscriptions.SubscriptionOptions
+import com.pubnub.chat.Channel
+import com.pubnub.chat.ChannelGroup
+import com.pubnub.chat.Message
+import com.pubnub.chat.internal.ChatInternal
+import com.pubnub.chat.internal.defaultGetMessageResponseBody
+import com.pubnub.chat.internal.error.PubNubErrorMessage.ERROR_HANDLING_ONMESSAGE_EVENT
+import com.pubnub.chat.internal.message.MessageImpl
+import com.pubnub.chat.types.GetChannelsResponse
+import com.pubnub.kmp.PNFuture
+import com.pubnub.kmp.createEventListener
+import com.pubnub.kmp.remember
+import com.pubnub.kmp.then
+import com.pubnub.kmp.thenAsync
+
+data class ChannelGroupImpl(
+    override val id: String,
+    override val chat: ChatInternal
+) : ChannelGroup {
+    companion object {
+        private val log = Logger.withTag("ChannelGroupImpl")
+    }
+
+    override fun listChannels(
+        filter: String?,
+        sort: Collection<PNSortKey<PNKey>>,
+        limit: Int?,
+        page: PNPage?
+    ): PNFuture<GetChannelsResponse> {
+        return chat.pubNub.listChannelsForChannelGroup(id).thenAsync { response ->
+            chat.getChannels(
+                createFinalFilterToApply(filter, response.channels),
+                sort,
+                limit,
+                page
+            )
+        }
+    }
+
+    private fun createFinalFilterToApply(currentFilter: String? = null, channelIdentifiers: List<String>): String? {
+        val channelsFilter = channelIdentifiers.joinToString(" || ") {
+            "channel.id == $it"
+        }
+        return currentFilter?.let {
+            it.plus(" && ($channelsFilter)")
+        } ?: run {
+            channelsFilter
+        }
+    }
+
+    override fun add(channels: List<Channel>): PNFuture<Unit> {
+        return chat.pubNub.addChannelsToChannelGroup(channels.map { it.id }, id).then {}
+    }
+
+    override fun add(channels: List<String>): PNFuture<Unit> {
+        return chat.pubNub.addChannelsToChannelGroup(channels, id).then {}
+    }
+
+    override fun remove(channels: List<Channel>): PNFuture<Unit> {
+        return chat.pubNub.removeChannelsFromChannelGroup(channels.map { it.id }, id).then {}
+    }
+
+    override fun remove(channels: List<String>): PNFuture<Unit> {
+        return chat.pubNub.removeChannelsFromChannelGroup(channels, id).then {}
+    }
+
+    override fun whoIsPresent(): PNFuture<Map<String, List<String>>> {
+        return chat.pubNub.hereNow(channelGroups = listOf(id)).then { response ->
+            response.channels.mapValues { it ->
+                it.value.occupants.map(PNHereNowOccupantData::uuid)
+            }
+        }
+    }
+
+    override fun streamPresence(callback: (userIds: Map<String, List<String>>) -> Unit): AutoCloseable {
+        val ids = mutableMapOf<String, MutableSet<String>>()
+        val future = whoIsPresent().then { whoIsPresentResponse ->
+            ids.putAll(whoIsPresentResponse.mapValues { it.value.toMutableSet() })
+            callback(whoIsPresentResponse)
+        }.then {
+            chat.pubNub.channel(id).subscription(SubscriptionOptions.receivePresenceEvents())
+                .also { subscription ->
+                    subscription.addListener(
+                        createEventListener(
+                            chat.pubNub,
+                            onPresence = { _, event ->
+                                val leaveUuids = event.leave ?: emptyList<String>()
+                                val joinUuids = event.join ?: emptyList<String>()
+                                val timedOutUuids = event.timeout ?: emptyList<String>()
+
+                                joinUuids.forEach { id ->
+                                    if (ids.contains(event.channel)) {
+                                        ids[event.channel]?.add(id)
+                                    } else {
+                                        ids[event.channel] = joinUuids.toMutableSet()
+                                    }
+                                }
+                                leaveUuids.forEach { id ->
+                                    ids[event.channel]?.remove(id)
+                                }
+                                timedOutUuids.forEach { id ->
+                                    ids[event.channel]?.remove(id)
+                                }
+
+                                when (event.event) {
+                                    "join" -> {
+                                        event.uuid.let { uuid ->
+                                            ids[event.channel]?.plus(uuid)
+                                        }
+                                    }
+
+                                    "leave", "timeout" -> {
+                                        event.uuid.let { uuid ->
+                                            ids[event.uuid]?.remove(uuid)
+                                        }
+                                    }
+                                }
+                                callback(ids.mapValues { it.value.toList() })
+                            }
+                        )
+                    )
+                    subscription.subscribe()
+                }
+        }.remember()
+
+        return AutoCloseable {
+            future.then { it.close() }
+        }
+    }
+
+    override fun connect(callback: (Message) -> Unit): AutoCloseable? {
+        val channelGroupEntity = chat.pubNub.channelGroup(id)
+        val subscription = channelGroupEntity.subscription()
+        val listener = createEventListener(
+            chat.pubNub,
+            onMessage = { _, pnMessageResult ->
+                if (pnMessageResult.publisher in chat.mutedUsersManager.mutedUsers) {
+                    return@createEventListener
+                }
+                try {
+                    if (
+                        (
+                            chat.config.customPayloads?.getMessageResponseBody?.invoke(
+                                pnMessageResult.message,
+                                pnMessageResult.channel,
+                                ::defaultGetMessageResponseBody
+                            )
+                                ?: defaultGetMessageResponseBody(pnMessageResult.message)
+                        ) == null
+                    ) {
+                        return@createEventListener
+                    }
+                    callback(MessageImpl.fromDTO(chat, pnMessageResult))
+                } catch (e: Exception) {
+                    log.e(throwable = e) { ERROR_HANDLING_ONMESSAGE_EVENT }
+                }
+            },
+        )
+        subscription.addListener(listener)
+        subscription.subscribe()
+
+        return subscription
+    }
+}
