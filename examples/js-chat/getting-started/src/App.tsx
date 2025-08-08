c94248550ce265a959b6c450fb392380fbf3b0d5
jguz-pubnub <102806147+jguz-pubnub@users.noreply.github.com>
2025-08-08 14:15:10 +0200

    Add getting started application for JS target (#207)

---
+++ examples/js-chat/getting-started/src/App.tsx
@@ -0,0 +1,134 @@
+import React, { useCallback, useEffect, useRef, useState } from "react"
+import { Channel, Chat, Message, MixedTextTypedElement, TimetokenUtils, User } from "@pubnub/chat"
+import "./App.css"
+
+const userData = [
+  {
+    id: "support-agent",
+    data: { name: "John (Support Agent)", custom: { initials: "SA", avatar: "#9fa7df" } },
+  },
+  {
+    id: "supported-user",
+    data: { name: "Mary Watson", custom: { initials: "MW", avatar: "#ffab91" } },
+  },
+]
+const randomizedUsers = Math.random() < 0.5 ? userData : userData.reverse()
+
+export default function App() {
+  const [chat, setChat] = useState<Chat>()
+  const [text, setText] = useState("")
+  const [users, setUsers] = useState<User[]>([])
+  const [channel, setChannel] = useState<Channel>()
+  const [messages, setMessages] = useState<Message[]>([])
+  const messageListRef = useRef<HTMLElement>(null)
+
+  async function handleSend(event: React.SyntheticEvent) {
+    event.preventDefault()
+    if (!text || !channel) return
+    await channel.sendText(text)
+    setText("")
+  }
+
+  useEffect(() => {
+    if (!messageListRef.current) return
+    messageListRef.current.scrollTop = messageListRef.current.scrollHeight
+  }, [messages])
+
+  useEffect(() => {
+    if (!channel) return
+    return channel.connect((message) => setMessages((messages) => [...messages, message]))
+  }, [channel])
+
+  useEffect(() => {
+    async function initalizeChat() {
+      const chat = await Chat.init({
+        publishKey: import.meta.env.VITE_PUBNUB_PUB_KEY,
+        subscribeKey: import.meta.env.VITE_PUBNUB_SUB_KEY,
+        userId: randomizedUsers[0].id,
+      })
+      const currentUser = await chat.currentUser.update(randomizedUsers[0].data)
+      const interlocutor =
+        (await chat.getUser(randomizedUsers[1].id)) ||
+        (await chat.createUser(randomizedUsers[1].id, randomizedUsers[1].data))
+      const { channel } = await chat.createDirectConversation({
+        user: interlocutor,
+        channelData: { name: "Support Channel" },
+      })
+      setChat(chat)
+      setUsers([currentUser, interlocutor])
+      setChannel(channel)
+    }
+
+    initalizeChat()
+  }, [])
+
+  const renderMessagePart = useCallback((messagePart: MixedTextTypedElement) => {
+    if (messagePart.type === "text") {
+      return messagePart.content.text
+    }
+    if (messagePart.type === "plainLink") {
+      return <a href={messagePart.content.link}>{messagePart.content.link}</a>
+    }
+    if (messagePart.type === "textLink") {
+      return <a href={messagePart.content.link}>{messagePart.content.text}</a>
+    }
+    if (messagePart.type === "mention") {
+      return <a href={`https://pubnub.com/${messagePart.content.id}`}>{messagePart.content.name}</a>
+    }
+
+    return ""
+  }, [])
+
+  if (!chat || !channel) return <p>Loading...</p>
+
+  return (
+    <main>
+      <header>
+        <h3>{channel.name}</h3>
+        <h3>{chat.currentUser.name}</h3>
+      </header>
+
+      <section className="message-list" ref={messageListRef}>
+        <ol>
+          {messages.map((message) => {
+            const user = users.find((user) => user.id === message.userId)
+            return (
+              <li key={message.timetoken}>
+                <aside style={{ background: String(user?.custom?.avatar) }}>
+                  {user?.custom?.initials}
+                </aside>
+                <article>
+                  <h3>
+                    {user?.name}
+                    <time>
+                      {TimetokenUtils.timetokenToDate(message.timetoken).toLocaleTimeString([], {
+                        timeStyle: "short",
+                      })}
+                    </time>
+                  </h3>
+                  <p>
+                    {message
+                      .getLinkedText()
+                      .map((messagePart: MixedTextTypedElement, i: number) => (
+                        <span key={String(i)}>{renderMessagePart(messagePart)}</span>
+                      ))}
+                  </p>
+                </article>
+              </li>
+            )
+          })}
+        </ol>
+      </section>
+
+      <form className="message-input" onSubmit={handleSend}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Send message"
+        />
+        <input type="submit" value="➔" onClick={handleSend} style={{ color: text && "#de2440" }} />
+      </form>
+    </main>
+  )
+}
